f5516bace8882f46ce7b959575220aca166a1657
Tom Dörr <tom.doerr@tum.de>
2021-11-15 20:02:09 +0100

    Update files

--- main.py
+++ main.py
@@ -1,47 +1,55 @@
+import os
+import sys
+import time
 import datetime
 import requests
 import json
-import sys
-import os
 
-def get_sunset_time(lat, lon):
-    """
-    This function gets the sunset time of the location based on the lat and lon
-    of the city.
-    """
-    url = "https://api.sunrise-sunset.org/json?lat={0}&lng={1}&formatted=0".format(lat, lon)
-    response = requests.get(url)
-    json_data = json.loads(response.text)
-    sunset = json_data["results"]["sunset"]
-    return sunset
-
-def get_time_from_sunset(sunset):
-    """
-    This function gets the time from the sunset time.
-    """
-    sunset_utc = datetime.datetime.strptime(sunset, "%Y-%m-%dT%H:%M:%S%z")
-    sunset_local = sunset_utc.astimezone()
-    return sunset_local.strftime("%H:%M")
-
-def get_location():
-    """
-    This function gets the location of the user.
-    """
-    url = "http://ipinfo.io/json"
-    response = requests.get(url)
-    json_data = json.loads(response.text)
-    lat = json_data["loc"].split(",")[0]
-    lon = json_data["loc"].split(",")[1]
+# get the timezone of your location based on your IP
+# https://stackoverflow.com/a/51800122
+def get_timezone():
+    send_url = 'http://ip-api.com/json'
+    r = requests.get(send_url)
+    j = json.loads(r.text)
+    lat = j['lat']
+    lon = j['lon']
     return lat, lon
 
-def main():
-    """
-    This function is the main function.
-    """
-    lat, lon = get_location()
-    sunset = get_sunset_time(lat, lon)
-    sunset_time = get_time_from_sunset(sunset)
-    print(sunset_time)
+# get the sunset time for your location
+def get_sunset(lat, lon):
+    send_url = 'https://api.sunrise-sunset.org/json?lat=' + str(lat) + '&lng=' + str(lon) + '&formatted=0'
+    r = requests.get(send_url)
+    j = json.loads(r.text)
+    sunset = j['results']['sunset']
+    return sunset
 
+# convert the time to your timezone
+def convert_time(sunset):
+    # convert datetime format to string
+    sunset_string = str(sunset)
+    # get the current time
+    current_time = datetime.datetime.now()
+    # convert the string to datetime format
+    sunset_datetime = datetime.datetime.strptime(sunset_string, '%Y-%m-%dT%H:%M:%S+00:00')
+    # calculate the timezone offset
+    timezone_offset = current_time - datetime.datetime.utcnow()
+    # convert the time to your timezone
+    converted_time = sunset_datetime + timezone_offset
+    # convert the datetime to string
+    converted_time_string = converted_time.strftime('%H:%M')
+    return converted_time_string
+
+# main function
+def main():
+    # get the timezone of your location
+    lat, lon = get_timezone()
+    # get the sunset time for your location
+    sunset = get_sunset(lat, lon)
+    # convert the sunset time to your timezone
+    converted_time = convert_time(sunset)
+    print(converted_time)
+
+# if this is the main thread of execution first load the model and then start the server
 if __name__ == "__main__":
+    # run the main function
     main()
